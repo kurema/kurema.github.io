247e77b59a2a4acdc0311cc3f77207a0cefc17d0
kurema <kurema_makoto_software@yahoo.co.jp>
2016-10-22 18:29:11 +0900

    test

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+var GithubApi = (function () {
+    function GithubApi() {
+        this.APIUrl = "https://api.github.com";
     }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
+    GithubApi.prototype.GetJSON = function (site, callback) {
+        GithubApi.GetJSONSimple(this.APIUrl + site, callback);
     };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
+    GithubApi.GetJSONSimple = function (url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status = 200) {
+                    callback(JSON.parse(xhr.responseText));
+                }
+            }
+        };
+        xhr.open("GET", url);
+        xhr.send();
     };
-    return Greeter;
+    GithubApi.prototype.GetUserJSON = function (user, callback) {
+        this.GetJSON("/users/" + user, callback);
+    };
+    return GithubApi;
 }());
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    var githubapi = new GithubApi();
+    githubapi.GetUserJSON("kurema", function (content) {
+        var githubCard = document.getElementById("githubCard");
+        var githubImgs = githubCard.getElementsByTagName("img");
+        for (var i = 0; i < githubImgs.length; i++) {
+            if (githubImgs[i].classList.contains("avatar")) {
+                githubImgs[i].setAttribute("src", content.avatar_url);
+            }
+        }
+        var githubUserLinks = githubCard.getElementsByTagName("a");
+        for (var i = 0; i < githubUserLinks.length; i++) {
+            if (githubUserLinks[i].classList.contains("userLink")) {
+                githubUserLinks[i].setAttribute("href", content.html_url);
+                githubUserLinks[i].innerText = content.name;
+            }
+        }
+    });
 };
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,28 +1,45 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+﻿class GithubApi {
+    APIUrl: string = "https://api.github.com";
+    GetJSON(site: string, callback: (content: any) => void) {
+        GithubApi.GetJSONSimple(this.APIUrl + site, callback);
     }
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    static GetJSONSimple(url: string, callback: (content: any) => void) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status = 200) {
+                    callback(JSON.parse(xhr.responseText));
+                }
+            }
+        };
+        xhr.open("GET", url);
+        xhr.send();
     }
 
-    stop() {
-        clearTimeout(this.timerToken);
+    GetUserJSON(user: string, callback: (content: any) => void) {
+        this.GetJSON("/users/" + user, callback);
     }
-
 }
 
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    var githubapi = new GithubApi();
+    githubapi.GetUserJSON("kurema", (content) => {
+        var githubCard = document.getElementById("githubCard");
+
+        var githubImgs = githubCard.getElementsByTagName("img");
+        for (var i = 0; i < githubImgs.length; i++) {
+            if (githubImgs[i].classList.contains("avatar")){
+                githubImgs[i].setAttribute("src", content.avatar_url);
+            }
+        }
+
+        var githubUserLinks = githubCard.getElementsByTagName("a");
+        for (var i = 0; i < githubUserLinks.length; i++) {
+            if (githubUserLinks[i].classList.contains("userLink")) {
+                githubUserLinks[i].setAttribute("href", content.html_url);
+                githubUserLinks[i].innerText = content.name;
+            }
+        }
+    });
 };
