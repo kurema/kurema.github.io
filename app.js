83f965493de72de142f91ff06035a2cd134c39a7
kurema <kurema_makoto_software@yahoo.co.jp>
2016-10-24 04:11:54 +0900

    Add modern footer.

--- app.js
+++ app.js
@@ -22,6 +22,86 @@ var GithubApi = (function () {
     };
     return GithubApi;
 }());
+var Footer = (function () {
+    function Footer() {
+    }
+    Footer.prototype.AddItem = function (item) {
+        var container = document.getElementById("footer_container");
+        container.appendChild(item.GetHtml());
+    };
+    return Footer;
+}());
+var FooterItem = (function () {
+    function FooterItem(Title) {
+        this.Title = Title;
+        this.Items = new Array();
+    }
+    FooterItem.prototype.GetHtml = function () {
+        var result = document.createElement("div");
+        result.className = "item";
+        var h5 = document.createElement("h5");
+        h5.appendChild(document.createTextNode(this.Title));
+        result.appendChild(h5);
+        var ul = document.createElement("ul");
+        for (var i = 0; i < this.Items.length; i++) {
+            var item = document.createElement("li");
+            item.appendChild(this.Items[i].GetHtml());
+            ul.appendChild(item);
+        }
+        result.appendChild(ul);
+        return result;
+    };
+    FooterItem.prototype.PushList = function (Text, Reference, Icon) {
+        this.Items.push(new FooterList(Text, Reference, Icon));
+    };
+    return FooterItem;
+}());
+var FooterList = (function () {
+    function FooterList(Text, Reference, Icon) {
+        this.Text = Text;
+        this.Reference = Reference;
+        this.Icon = Icon;
+    }
+    FooterList.prototype.GetHtml = function () {
+        var icon = document.createElement("img");
+        icon.src = this.Icon;
+        if (this.Reference != null) {
+            var result = document.createElement("a");
+            result.setAttribute("href", this.Reference);
+            if (this.Icon != null) {
+                result.appendChild(icon);
+            }
+            result.appendChild(document.createTextNode(this.Text));
+            return result;
+        }
+        else {
+            var result2 = document.createElement("span");
+            if (this.Icon != null) {
+                result2.appendChild(icon);
+            }
+            result2.appendChild(document.createTextNode(this.Text));
+            return result2;
+        }
+    };
+    return FooterList;
+}());
 window.onload = function () {
+    var footer = new Footer();
+    var item = new FooterItem("Follow");
+    item.PushList("Github", "https://github.com/kurema", "image/GitHub-Mark-Light-64px.png");
+    item.PushList("Twitter", "https://twitter.com/kurema_makoto", "image/Twitter_Logo_White_On_Blue.svg");
+    item.PushList("Blog", "https://kuremako.wordpress.com", "https://s.w.org/about/images/logos/wordpress-logo-32-blue.png");
+    item.PushList("Youtube", "https://www.youtube.com/channel/UCRXOgsw-LUdgPSN95myPw7g", "image/YouTube-social-circle_red_48px.png");
+    footer.AddItem(item);
+    var projectItem = new FooterItem("Projects");
+    var githubApi = new GithubApi();
+    githubApi.GetUserJSON("kurema", function (content) {
+        GithubApi.GetJSONSimple(content.repos_url, function (contentRepo) {
+            for (var j = 0; j < contentRepo.length; j++) {
+                projectItem.PushList(contentRepo[j].name, contentRepo[j].html_url);
+            }
+            footer.AddItem(projectItem);
+        });
+    });
 };
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -22,5 +22,103 @@
     }
 }
 
+class Footer {
+
+    AddItem(item: FooterItem) {
+        var container = document.getElementById("footer_container");
+        container.appendChild(item.GetHtml());
+    }
+
+}
+
+class FooterItem {
+    Title: string;
+    Items: FooterList[];
+
+    constructor(Title?: string) {
+        this.Title = Title;
+        this.Items = new Array();
+    }
+
+
+    GetHtml(): HTMLElement {
+        var result = document.createElement("div");
+        result.className = "item";
+
+        var h5 = document.createElement("h5");
+        h5.appendChild(document.createTextNode(this.Title));
+        result.appendChild(h5);
+
+        var ul = document.createElement("ul");
+        for (var i = 0; i < this.Items.length; i++) {
+            var item = document.createElement("li");
+            item.appendChild(this.Items[i].GetHtml());
+            ul.appendChild(item);
+        }
+        result.appendChild(ul);
+
+        return result;
+    }
+
+    PushList(Text: string, Reference?: string, Icon?: string) {
+        this.Items.push(new FooterList(Text, Reference, Icon));
+    }
+    
+}
+
+class FooterList {
+    Text: string;
+    Reference: string;
+    Icon: string;
+
+    constructor(Text: string, Reference?: string, Icon?: string) {
+        this.Text = Text;
+        this.Reference = Reference;
+        this.Icon = Icon;
+    }
+
+    GetHtml(): HTMLElement {
+        var icon = document.createElement("img");
+        icon.src = this.Icon;
+
+        if (this.Reference != null) {
+            var result = document.createElement("a");
+            result.setAttribute("href", this.Reference);
+            if (this.Icon != null) {
+                result.appendChild(icon);
+            }
+            result.appendChild(document.createTextNode(this.Text));
+            return result;
+        } else {
+            var result2 = document.createElement("span");
+            if (this.Icon != null) {
+                result2.appendChild(icon);
+            }
+            result2.appendChild(document.createTextNode(this.Text));
+            return result2;
+        }
+    }
+}
+
 window.onload = () => {
+    var footer = new Footer();
+
+    var item = new FooterItem("Follow");
+    item.PushList("Github", "https://github.com/kurema","image/GitHub-Mark-Light-64px.png");
+    item.PushList("Twitter", "https://twitter.com/kurema_makoto", "image/Twitter_Logo_White_On_Blue.svg");
+    item.PushList("Blog", "https://kuremako.wordpress.com", "https://s.w.org/about/images/logos/wordpress-logo-32-blue.png");
+    item.PushList("Youtube", "https://www.youtube.com/channel/UCRXOgsw-LUdgPSN95myPw7g", "image/YouTube-social-circle_red_48px.png");
+
+    footer.AddItem(item);
+
+    var projectItem = new FooterItem("Projects");
+    var githubApi = new GithubApi();
+    githubApi.GetUserJSON("kurema", (content) => {
+        GithubApi.GetJSONSimple(content.repos_url, (contentRepo) => {
+            for (var j = 0; j < contentRepo.length; j++) {
+                projectItem.PushList(contentRepo[j].name, contentRepo[j].html_url);
+            }
+            footer.AddItem(projectItem);
+        });
+    });
 };
