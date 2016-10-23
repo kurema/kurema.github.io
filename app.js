8ed3afa580e4765914dec9f1585abfc731dac50b
kurema <kurema_makoto_software@yahoo.co.jp>
2016-10-24 01:41:10 +0900

    quick sync

--- app.js
+++ app.js
@@ -23,22 +23,5 @@ var GithubApi = (function () {
     return GithubApi;
 }());
 window.onload = function () {
-    var githubapi = new GithubApi();
-    githubapi.GetUserJSON("kurema", function (content) {
-        var githubCard = document.getElementById("githubCard");
-        var githubImgs = githubCard.getElementsByTagName("img");
-        for (var i = 0; i < githubImgs.length; i++) {
-            if (githubImgs[i].classList.contains("avatar")) {
-                githubImgs[i].setAttribute("src", content.avatar_url);
-            }
-        }
-        var githubUserLinks = githubCard.getElementsByTagName("a");
-        for (var i = 0; i < githubUserLinks.length; i++) {
-            if (githubUserLinks[i].classList.contains("userLink")) {
-                githubUserLinks[i].setAttribute("href", content.html_url);
-                githubUserLinks[i].innerText = content.name;
-            }
-        }
-    });
 };
 //# sourceMappingURL=app.js.map
